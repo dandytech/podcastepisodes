f6e5b596a4bd92b38e85246d6153a8c08662813f
amaechinwankwo <damaechi@rocketmail.com>
2025-04-17 21:58:44 +0100

    Initial commit

--- components/ShareButton.tsx
+++ components/ShareButton.tsx
@@ -4,28 +4,26 @@ import gift from "@/public/icons/gift.png";
 import share from "@/public/icons/share.png";
 
 interface ButtonProps {
-  children: React.ReactNode;
   onClick?: () => void; // Make onClick optional
 }
 
-const ShareButton: React.FC<ButtonProps> = ({ children, onClick }) => {
+const ShareButton: React.FC<ButtonProps> = ({ onClick }) => {
   return (
     <div className="flex gap-1 items-center">
-      {" "}
-      <button className="p-3 rounded-full  w-[50px] h-[50px]">
+      <button className="p-3 rounded-full w-[50px] h-[50px]" onClick={onClick}>
         <Image
           src={share}
           quality={100}
           className="object-cover object-top w-full"
-          alt="logo"
+          alt="Share"
         />
       </button>
-      <button className="p-3 rounded-full  w-[50px] h-[50px]">
+      <button className="p-3 rounded-full w-[50px] h-[50px]" onClick={onClick}>
         <Image
           src={gift}
           quality={100}
           className="object-cover object-top w-full"
-          alt="logo"
+          alt="Gift"
         />
       </button>
     </div>
